b7c047f9e2ed007693926bb8418e36502547aac0
lianzi liu <lianziliu.sugar@gmail.com>
2022-08-31 15:38:32 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(name,surpr) {
+    let message = [];
+    for(let i = 0; i < name.length; i++ ) {
+         message.push(`Thank you, ${name[i]}, for the wonderful ${surpr} gift!`);
+    }
+return message;
+}
+
+
+function countDown(number){
+    while (number >=  0){
+        console.log(number);
+        number --;
+    }
+}
